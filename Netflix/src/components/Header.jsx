7785a1946038780a7a25409efe5e3723575dc082
sumit007ind <sharmaraj81808@gmail.com>
2025-10-28 23:42:31 +0530

    Update Header.jsx

--- Netflix/src/components/Header.jsx
+++ Netflix/src/components/Header.jsx
@@ -4,11 +4,7 @@ import { IoIosArrowDropdownCircle } from "react-icons/io";
 const Header = () => {
   return (
     <div className="bg-gradient-to-b absolute  from-black flex items-center justify-between p-4 w-full">
-      <img
-        className="w-36"
-        src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png"
-        alt="netflix logo"
-      />
+     
 
       <div className="flex items-center text-white">
         <IoIosArrowDropdownCircle color="white" />
