c85181649e082a774dbddfbb8210bf3b5f40a629
sumit007ind <sharmaraj81808@gmail.com>
2025-11-17 23:55:36 +0530

    Update userSchema.js

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -1,14 +1,3 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    FullName: { type: String, 
-        required: true },
-  Email: { type: String,
-     required: true, 
-     unique: true },
-  Password: { type: String,
-     required: true }
-
- },{timestamps: true});
-
-export default mongoose.model("User", userSchema);
+c
