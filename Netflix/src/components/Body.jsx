b2ab655e0f83384eedd62dfb4eb97dec37f49551
sumit007ind <sharmaraj81808@gmail.com>
2025-12-03 22:39:14 +0530

    Update Body.jsx

--- Netflix/src/components/Body.jsx
+++ Netflix/src/components/Body.jsx
@@ -3,17 +3,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './Login';
 import Browse from './Browse';
 
-const Body = () => {
-  const approuter = createBrowserRouter([
-    {
-      path: '/',
-      element: <Login />,
-    },
-    {
-      path: '/browse', 
-      element: <Browse />,
-    },
-  ]);
 
   return (
     <div>
